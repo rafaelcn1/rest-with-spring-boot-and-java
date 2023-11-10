42c629157f4937857f57440bc1ca23917387a14e
Rafael Cunha <rafaelcn1@hotmail.com>
2023-11-10 16:42:53 -0300

    fix: ajuste greeting controller

--- rest-with-spring-boot-and-java/src/main/java/com/rafael/controllers/GreetingController.java
+++ rest-with-spring-boot-and-java/src/main/java/com/rafael/controllers/GreetingController.java
@@ -1,10 +1,12 @@
-package com.rafael;
+package com.rafael.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.Greeting;
 
 @RestController
 public class GreetingController {
