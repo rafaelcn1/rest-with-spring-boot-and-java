4eab032845d23fcb91f3f94ff6f656fd865b3d57
Rafael Cunha <rafaelcn1@hotmail.com>
2023-11-29 14:52:56 -0300

    feat: habilitando core atravez de controller

--- rest-with-spring-boot-and-java/src/main/java/com/rafael/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/com/rafael/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.dtos.PersonDTO;
-import com.rafael.model.Person;
 import com.rafael.services.PersonService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -33,11 +33,13 @@ public class PersonController {
 		return this.personService.listAll();
 	}
 
+	@CrossOrigin(origins = {"http://localhost:8080"})
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public PersonDTO findById(@PathVariable(value = "id") Long id) {
 		return this.personService.findById(id);
 	}
 
+	@CrossOrigin(origins = {"http://localhost:8080"})
 	@PostMapping(
 			produces = MediaType.APPLICATION_JSON_VALUE, //Vai produzir um JSON
 			consumes = MediaType.APPLICATION_JSON_VALUE) //Vai consumir um JSON
